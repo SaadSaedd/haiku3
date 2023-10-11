721b145f7f4df420143efa293caa19e31e663a13
saad <37354@ma-web.nl>
2023-10-11 10:09:05 +0200

    haiku3

--- haiku.js
+++ haiku.js
@@ -1,6 +1,24 @@
 
 
 //plak hier de haiku functions onder elkaar
+function Haiku1(){
+    console.log("haiku by:","saad");
+    console.log("Laag over de spoorlijn vliegen de wilde ganzen door maan beschenen.");
+    console.log("Dikke huisjesslak, ook jij beklimt de Fuji maar langzaam, langzaam.");
+    console.log("Velden geel koolzaad de maan staat in het oosten de zon in westen.");
+}
+function Haiku2(){
+    console.log("haiku by:","abdul");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function Haiku3(){
+    console.log("haiku by:","damain");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+} 
 
 
 
